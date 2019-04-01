767ea4596d5f3ed19e11ab1ed1fc7acb0c62298a
ZJQi <zhangjiaqi77777@outlook.com>
2019-04-01 11:45:19 +0800

    新增对多key的存在性判定和key的删除清空操作

--- redis_others.py
+++ redis_others.py
@@ -12,12 +12,28 @@ from redis_pool import rpool
 
 conn = redis.Redis(connection_pool=rpool)
 
-# 判定key是否存在，存在返回1， 不存在返回0。
-print(conn.exists('k1'))
+# 判定key是否存在,放回存在的key的个数，如果只是test一个key，那么存在返回1, 不存在返回0；如果多个，这是多个中存在的。
+print(conn.exists('k1'))  # 可以检查多个key是否存在，conn.exists('k1', 'k2')
+# 判断多个存在可以通过len(keys) == conn.exists(*keys) 判定是否都存在
 
 # 设置key的有效期
 conn.set('k120', 20, ex=5)  # ex设置有效时长单位是秒， px设置有效时长，单位是毫秒
 print(conn.get('k120'))
+
+# 获取所有key
+conn.set('k130', 40)
+print(conn.keys())
+print(conn.keys(pattern='k*'))
+
+# 模糊匹配key，得到获取key的生成器
+for k in conn.scan_iter('k*'):
+    print(k)
+
+# 删除一个key
+conn.delete('k120')
+
+# 清空所有的key，禁止且慎重使用，做了解
+conn.flushall()
 
 if __name__ == '__main__':
     pass
